2c18b30ca0ae5039409febe858ce85437a4d7602
Temirlan Nurmambetov <62405040+TemirlanN-dev@users.noreply.github.com>
2022-10-05 01:12:00 -0600

    Delete TemirlanNurmambetov/Robot FTC directory

--- TemirlanNurmambetov/Robot FTC/src/Robot.java
+++
@@ -1,9 +0,0 @@
-public class Robot {
-
-    public static void main(String[] args) {
-        Motor leftR = new Motor(123);
-        Motor rightR = new Motor(321);
-        Controller keyboard = new Controller();
-        keyboard.takeInput(leftR, rightR);
-    }
-}
